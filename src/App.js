ccd1aa1cb1d605b3e68a1feb5ccd02812c827fbb
Roman Tarnakin <roman_tarnakin@epam.com>
2022-12-11 17:06:38 +0300

    add 2 components and some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+const expenseItems = [
+  {
+    date: new Date(2000, 2, 3),
+    title: "Book",
+    price: 10,
+  },
+  {
+    date: new Date(2010, 3, 4),
+    title: "PC",
+    price: 1000,
+  },
+  {
+    date: new Date(2020, 4, 5),
+    title: "Plane",
+    price: 10000,
+  },
+];
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <p>Hello!!!</p>
+      {expenseItems.map(({ date, title, price }) => {
+        return <ExpenseItem date={date} title={title} price={price} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
